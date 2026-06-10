52553fabaef532eeff6ec3ca3e9ae74bb7addfe7
quangnxse180723 <quangnxse180723@fpt.edu.vn>
2026-06-10 13:18:49 +0700

    Initial commit: Setup Spring Boot structure for VSL Project

---
+++ src/main/java/com/vslbackend/VslBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vslbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VslBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VslBackendApplication.class, args);
+    }
+
+}
